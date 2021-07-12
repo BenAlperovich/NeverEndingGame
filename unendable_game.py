c63be3c02babe551552d8c5c00b0ecc1c947b4d1
BenAlperovich <87313828+BenAlperovich@users.noreply.github.com>
2021-07-12 08:40:20 -0400

    Update unendable_game.py

--- unendable_game.py
+++ unendable_game.py
@@ -12,7 +12,7 @@ text=font.render("0",True,(255,255,255))
 text_rect=text.get_rect(center=(w-50,50))
 
 
-hacksMode=True #Super duper secret
+hacksMode=False #Super duper secret
 
 class Platform():
     def __init__(self,x,y,width,height,color,_type="normal"):
@@ -55,6 +55,7 @@ upCountdown=defUpCountDown
 defJumpsLeft=2
 jumpsLeft=defJumpsLeft
 score=0
+paused=False
 
 platforms=[Platform(w//2,playerY+int(playerRad*1.5),200,50,(255,0,204)),
            Platform(w//2+400,playerY+int(playerRad*1.5),200,50,(255,0,204)),
@@ -65,11 +66,14 @@ input("""
 You are the circle. Jump from platform to platform and stay alive as long as possible.
 Press spacebar to jump
 Press l to make you move faster horizontally
+Press p to pause
 
 (Press enter to continue)
 """)
 screen=pygame.display.set_mode((w,h))
 
+
+#main loop
 while True:
     if startFalling:
         fall=True
@@ -79,7 +83,7 @@ while True:
             sys.exit()
             quit()
         elif event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_SPACE:
+            if event.key==pygame.K_SPACE and not paused:
                 jumpsLeft-=1
                 if jumpsLeft>0:
                     upMotion=True
@@ -87,37 +91,41 @@ while True:
                     startFalling=True
                     movingLeft=True
                 
-            elif event.key==pygame.K_l:
+            elif event.key==pygame.K_l and not paused:
                 moveFaster=True
+
+            elif event.key==pygame.K_p:
+                paused=not paused
                 
-            if hacksMode:
+            if hacksMode and not paused:
                 if event.key==pygame.K_a: playerY+=60; fall=True
                 elif event.key==pygame.K_d: playerY-=60
                 elif event.key==pygame.K_q: playerY=1234
                 
-        elif event.type == pygame.KEYUP:
+        elif event.type == pygame.KEYUP and not paused:
             if event.key == pygame.K_l:
                 moveFaster=False
                 
     screen.fill((0,0,0))
 
     time.sleep(0.03)
-    if upCountdown>0 and upMotion:
-        upCountdown-=1
-        playerY-=15
-    else:
-        upCountdown=defUpCountDown
-        upMotion=False
+    if not paused:
+        if upCountdown>0 and upMotion:
+            upCountdown-=1
+            playerY-=15
+        else:
+            upCountdown=defUpCountDown
+            upMotion=False
     
     for i in range(0,len(platforms)):
         platform=platforms[i]
 
-        platform.runTimePrecedures()
+        if not paused: platform.runTimePrecedures()
         
-        if movingLeft:
+        if movingLeft and not paused:
             platform.x-=(abs(moveAmount)+abs(int(moveFaster))*2)
             
-        if platform.x+platform.width<=platform.width or platform.width+platform.x<playerX:
+        if (platform.x+platform.width<=platform.width or platform.width+platform.x<playerX) and not paused:
             deletePlatforms.append(i)
 
         rect=pygame.draw.rect(screen,platform.color,(platform.x-50,platform.y,platform.width,platform.height))
@@ -125,14 +133,14 @@ while True:
 
         #print(abs(player.bottom - rect.top),player.x//2 > rect.left,player.x//2 < rect.right)
 
-        if (not rect.colliderect(player)) and fall and i==0:
+        if (not rect.colliderect(player)) and fall and i==0 and not paused:
             playerY+=5
-        if rect.colliderect(player):
+        if rect.colliderect(player) and not paused:
             jumpsLeft=defJumpsLeft
             playerY=rect.top-playerRad
             if upMotion:
                 playerY=rect.top-playerRad
-            """if platform.type=="hole":
+            """if platform.type=="hole": and 
                 platform.timeleft-=1
                 if platform.timeleft==0:
                     deletePlatforms.append(i)"""
@@ -148,6 +156,10 @@ while True:
     deletePlatforms=[]
 
     screen.blit(text,text_rect)
+    if paused:
+        pauseText=font.render("Paused",True,(255,255,255))
+        pauseText_rect=pauseText.get_rect(center=(w//2,h//2))
+        screen.blit(pauseText,pauseText_rect)
 
     pygame.display.update()
 
@@ -155,4 +167,5 @@ while True:
         print("\n"*10)
         print("you lost, try to win next time!\n By the way, you got a score of ",end="")
         print(score)
+        pygame.quit()
         sys.exit()
